d19f59fdd698bb558c5adac3ad37a6bf2391abd0
Jahenrtz <info@jhanrahan.com.au>
2018-05-13 14:26:52 +0800

    fixed spelling errors

--- src/hello-phaser.js
+++ src/hello-phaser.js
@@ -18,27 +18,23 @@ var game = new Phaser.Game( config );
 
 function preload () {
 	// this needs to be pulled from wp_options:
-	this.load.setBaseUrl( 'http://dev.unknwn.asia' );
+	this.load.setBaseURL( 'http://dev.unknwn.asia' );
 	this.load.image( 'sky', 'assets/skies/space3.png' );
-	this.load.image( 'logo', 'assets/sprites/red.png' );
+	this.load.image( 'logo', 'assets/sprites/phaser3-logo.png' );
+	this.load.image( 'red', 'assets/sprites/red.png' );
 }
 
 function create () {
 	this.add.image( 400, 300, 'sky' );
-
 	var particles  = this.add.particles( 'red' );
-
 	var emitter = particles.createEmitter ( {
-		speed: 100;
+		speed: 100,
 		scale: { start: 1, end: 0 },
 		blendMode: 'ADD'
 	} );
-
 	var logo = this.physics.add.image( 400, 100, 'logo' );
-
 	logo.setVelocity( 100, 200 );
 	logo.setBounce( 1, 1 );
 	logo.setColliderWorldBounds( true );
-
 	emitter.startFollow( logo );
 }
