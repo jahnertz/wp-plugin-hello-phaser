a75ba429def7e23aedba96e16102aa31c7ed3d44
Jahenrtz <info@jhanrahan.com.au>
2018-05-13 13:38:44 +0800

    enqueued phaser library

--- class.hello-phaser.php
+++ class.hello-phaser.php
@@ -8,12 +8,18 @@
 		$options = get_option( HELLO_PHASER__OPTIONS );
 		isset( self::$debug_messages ) ?: ( $debug_messages = array() );
 		isset( $options[ 'initiated' ] ) ?: ( $this->initiate() );
+		add_action( 'wp_enqueue_scripts', array( get_called_class(), 'enqueue_user_scripts' ) );
 	}
 
 	public function initiate () {
 		$options = get_option( HELLO_PHASER__OPTIONS );
 		$options[ 'initiated' ] = true;
 		update_option( HELLO_PHASER__OPTIONS, $options );
+	}
+
+	public static function enqueue_user_scripts () {
+			wp_register_script( 'phaser', HELLO_PHASER__PLUGIN_URL . 'node_modules/phaser/dist/phaser.min.js', false );
+			wp_enqueue_script( 'phaser' );
 	}
 
 	public static function debug ( $message ) {
--- hello-phaser.php
+++ hello-phaser.php
@@ -13,6 +13,7 @@ Author URI: https://jhanrahan.com.au
 
 define( 'HELLO_PHASER__DEBUG_MODE', true );
 define( 'HELLO_PHASER__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'HELLO_PHASER__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'HELLO_PHASER__OPTIONS', 'hello_phaser_options' );
 
 require_once( HELLO_PHASER__PLUGIN_DIR . 'class.hello-phaser.php' );
