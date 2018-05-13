d61a8fc2025a012127bbc145daddd0f13d4a8456
Jahenrtz <info@jhanrahan.com.au>
2018-05-13 15:01:23 +0800

    scripts enqueued on shortcode only now.

--- class.hello-phaser.php
+++ class.hello-phaser.php
@@ -8,9 +8,11 @@
 		$options = get_option( HELLO_PHASER__OPTIONS );
 		isset( self::$debug_messages ) ?: ( $debug_messages = array() );
 		isset( $options[ 'initiated' ] ) ?: ( $this->initiate() );
-		add_action( 'wp_enqueue_scripts', array( get_called_class(), 'enqueue_user_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( get_called_class(), 'register_user_scripts' ) );
 		function hello_phaser_shortcode_func ( $atts ) {
-			$script = '<div id="hello-phaser-container"></div><script src="' . HELLO_PHASER__PLUGIN_URL . 'src/hello-phaser.js" ></script>'; 
+			wp_enqueue_script( 'phaser' );
+			wp_enqueue_script( 'hello-phaser' );
+			$script = '<div id="hello-phaser-container"></div>';
 			return $script;
 		}
 		add_shortcode( 'hello-phaser', 'hello_phaser_shortcode_func' );
@@ -21,12 +23,10 @@
 		$options[ 'initiated' ] = true;
 		update_option( HELLO_PHASER__OPTIONS, $options );
 	}
-
-	public static function enqueue_user_scripts () {
+	
+	public static function register_user_scripts () {
 			wp_register_script( 'phaser', HELLO_PHASER__PLUGIN_URL . 'node_modules/phaser/dist/phaser.min.js', array(), false );
 			wp_register_script( 'hello-phaser', HELLO_PHASER__PLUGIN_URL . 'src/hello-phaser.js', array( 'phaser' ), false );
-			wp_enqueue_script( 'phaser' );
-			// wp_enqueue_script( 'hello-phaser' );
 	}
 
 	public static function debug ( $message ) {
