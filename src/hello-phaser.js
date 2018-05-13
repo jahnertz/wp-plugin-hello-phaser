85a118a55c2c842a4cb3221b8897f2ebeef9c566
Jahenrtz <info@jhanrahan.com.au>
2018-05-13 14:33:56 +0800

    added assets

--- src/hello-phaser.js
+++ src/hello-phaser.js
@@ -18,15 +18,15 @@ var game = new Phaser.Game( config );
 
 function preload () {
 	// this needs to be pulled from wp_options:
-	this.load.setBaseURL( 'http://dev.unknwn.asia' );
-	this.load.image( 'sky', 'assets/skies/space3.png' );
+	this.load.setBaseURL( 'http://dev.unknwn.asia/wp-content/plugins/wp-plugin-hello-phaser/src/' );
+	this.load.image( 'sky', 'assets/skies/wtf.png' );
 	this.load.image( 'logo', 'assets/sprites/phaser3-logo.png' );
-	this.load.image( 'red', 'assets/sprites/red.png' );
+	this.load.image( 'particle', 'assets/sprites/particle1.png' );
 }
 
 function create () {
 	this.add.image( 400, 300, 'sky' );
-	var particles  = this.add.particles( 'red' );
+	var particles  = this.add.particles( 'particle' );
 	var emitter = particles.createEmitter ( {
 		speed: 100,
 		scale: { start: 1, end: 0 },
@@ -35,6 +35,6 @@ function create () {
 	var logo = this.physics.add.image( 400, 100, 'logo' );
 	logo.setVelocity( 100, 200 );
 	logo.setBounce( 1, 1 );
-	logo.setColliderWorldBounds( true );
+	logo.setCollideWorldBounds( true );
 	emitter.startFollow( logo );
 }
