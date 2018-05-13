0eaf86e0eb09c008b33991c35c0ca90a62122676
Jahenrtz <info@jhanrahan.com.au>
2018-05-13 12:50:13 +0800

    added hello-phaser class and hooked init method to init event

---
+++ class.hello-phaser.php
@@ -0,0 +1,20 @@
+<?php
+ class Hello_Phaser {
+
+	private static $debug_messages;
+
+	public function init () {
+		// TODO: include Phaser library
+		$options = get_option( HELLO_PHASER__OPTIONS );
+		isset( self::$debug_messages ) ?: ( $debug_messages = array() );
+		isset( $options[ 'initiated' ] ) ?: ( $this->initiate() );
+	}
+
+	public function initiate () {
+		// TODO: initiate the object.
+	}
+
+	public static function debug ( $message ) {
+		array_push( self::$debug_messages, $message );
+	}
+}
---
+++ hello-phaser.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package hello-phaser
+ * @version 1.0.0
+/*
+Plugin Name: hello-phaser
+Plugin URI: https://github.com/jahnertz/wp-plugin-hello-phaser 
+Description: A simple implementation of the Phaser game library in a Wordpress plugin.
+Author: Jordan Han
+Version: 1.0.0
+Author URI: https://jhanrahan.com.au
+*/
+
+define( 'HELLO_PHASER__DEBUG_MODE', true );
+define( 'HELLO_PHASER__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'HELLO_PHASER__OPTIONS', 'hello_phaser_options' );
+
+require_once( HELLO_PHASER__PLUGIN_DIR . 'class.hello-phaser.php' );
+
+$hello_phaser = new Hello_Phaser;
+add_action( 'init', array( &$hello_phaser, 'init' ), 1 );
